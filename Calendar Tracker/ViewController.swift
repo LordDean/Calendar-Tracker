bac49bfc51f64b71f7c6890f7b9c3fed8c5b4873
LordDean <dean.mollica@gmail.com>
2018-02-11 13:44:47 +1100

    Basic Month Navigation Added basic navigation between months, which updates the Navigation Bar title

--- Calendar Tracker/ViewController.swift
+++ Calendar Tracker/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    var currentMonth = Calendar.current.component(.month, from: Date()) - 1
+    var currentYear = Calendar.current.component(.year, from: Date())
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateMonth()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .rewind, target: self, action: #selector(navButtonPressed))
+        navigationItem.leftBarButtonItem?.tag = 1
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .fastForward, target: self, action: #selector(navButtonPressed))
+        navigationItem.rightBarButtonItem?.tag = 2
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func navButtonPressed(_ sender: UIBarButtonItem) {
+        if sender.tag == 1 {
+            updateMonth(-1)
+        } else {
+            updateMonth(1)
+        }
+        
     }
-
-
+    
+    func updateMonth(_ change: Int = 0) {
+        currentMonth += change
+        if currentMonth < 0 {
+            currentMonth = 11
+            currentYear -= 1
+        } else if currentMonth > 11 {
+            currentMonth = 0
+            currentYear += 1
+        }
+        
+        title = monthNames[currentMonth] + " " + String(currentYear)
+    }
 }
 
